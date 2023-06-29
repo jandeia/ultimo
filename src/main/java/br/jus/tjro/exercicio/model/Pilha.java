a4cb8f9f7df1072a40c72f78cba38f90c89ea76e
jandeia <jandeiavv@gmail.com>
2023-06-29 11:23:49 -0400

    testes unitarios

---
+++ src/main/java/br/jus/tjro/exercicio/model/Pilha.java
@@ -0,0 +1,35 @@
+package br.jus.tjro.exercicio.model;
+
+public class Pilha {
+
+	private Object[] elementos;
+	private int quantidade = 0;
+	
+	public Pilha(int maximo) {
+		elementos = new Object[maximo];
+	}
+
+	public boolean estaVazia() {
+		return quantidade == 0;
+	}
+
+	public Object tamanho() {
+		return quantidade;
+	}
+
+	public void empilha(Object elemento) {
+		this.elementos[quantidade] = elemento;
+		quantidade ++;
+	}
+
+	public Object topo() {
+		return elementos[quantidade-1];
+	}
+
+	public Object desempilha() {
+		Object topo = topo();
+		quantidade --;
+		return topo;
+	}
+
+}
